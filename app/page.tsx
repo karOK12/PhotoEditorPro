21e04e3d971ccb64aeeb78a3bd72b3cf9366c35f
اسمك <@example.com>
2026-09-07 00:06:29 +0300

    fix registration buttons layout

--- app/page.tsx
+++ app/page.tsx
@@ -464,7 +464,7 @@ export default function Home() {
 
           <div
             style={{
-              minHeight: registrationCompleted ? "0px" : "102px",
+              minHeight: "102px",
             }}
           >
           {authStateLoaded && !registrationCompleted && (

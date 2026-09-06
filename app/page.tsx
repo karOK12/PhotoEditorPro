61be525142dd00a9810bca2ec00a72e25e833ac9
اسمك <@example.com>
2026-09-06 23:43:27 +0300

    fix registration buttons visibility

--- app/page.tsx
+++ app/page.tsx
@@ -462,7 +462,7 @@ export default function Home() {
           </div>
         )}
 
-          {authStateLoaded && !registrationCompleted && (
+          {!registrationCompleted && (
             <>
         <div
           style={{
